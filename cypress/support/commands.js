4f850885cff2a608320cd09297592e421c965677
NataliaKondr <kondrateva861216@gmail.com>
2025-07-18 13:35:53 +0300

    changes in test cases of registration and login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,15 +24,54 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import { locators as loc } from '../support/locators';
-Cypress.Commands.add('openHomePage', () => {  
-    cy.visit('/') // Открытие главной страницы  
-    cy.contains('AutomationExercise') // Проверка, что страница загрузилась
+import {LoginPage, RegistrationPage} from '../pages/LoginPage'
+
+Cypress.Commands.add('openHomePage', () => {
+  cy.visit('/')
+  cy.contains('AutomationExercise')
 });
-Cypress.Commands.add('openTestCasesPage', () => {  
-    cy.contains(loc.HomePageLocators.testCasesLink, 'Test Cases').click(); 
-     cy.contains("Test Cases")
+Cypress.Commands.add('openTestCasesPage', () => {
+  cy.contains(loc.HomePageLocators.testCasesLink, 'Test Cases').click();
+  cy.contains("Test Cases")
 });
-Cypress.Commands.add('openLoginPage', () => {  
-    cy.get(loc.HomePageLocators.signupLoginLink).click() 
-    cy.contains('New User Signup!')
+
+Cypress.Commands.add('openLoginPage', () => {
+  cy.get(loc.HomePageLocators.signupLoginLink).click()
+  cy.contains('New User Signup!')
 });
+
+Cypress.Commands.add('registerUser', (user) => {
+  cy.openHomePage();
+  cy.openLoginPage();
+
+  const loginPage = new LoginPage();
+  const registrationPage = new RegistrationPage();
+
+  loginPage.enterName(user.fullName);
+  loginPage.enterEmail(user.email);
+  loginPage.clickSignup();
+
+  registrationPage.fillBasicInfo(user.fullName, user.password, user.details.dateOfBirth);
+  registrationPage.fillAddressInfo(user.firstName, user.lastName, user.details);
+  registrationPage.submitRegistration();
+
+  // Сохраняем данные пользователя в JSON-файл
+  cy.writeFile('cypress/fixtures/registeredUser.json', user);
+});
+
+Cypress.Commands.add('openLoginPageAccount', () => {
+  cy.get(loc.HomePageLocators.signupLoginLink).click()
+  cy.contains('Login to your account')
+});
+
+Cypress.Commands.add('loginAccount', () => {
+  cy.fixture('registeredUser').then(({ email, password, fullName }) => {
+    cy.get(loc.LoginPageLocators.emailInput).type(email);
+    cy.get(loc.LoginPageLocators.passwordInput).type(password);
+    cy.get(loc.LoginPageLocators.loginButton).click();
+    cy.contains(`Logged in as ${fullName}`).should('be.visible');
+  });
+});
+
+
+
--- cypress/support/generateUserData.js
+++ cypress/support/generateUserData.js
@@ -1,26 +1,45 @@
 import { faker } from '@faker-js/faker';
-export function generateEmail(firstName, lastName) {  
-    return faker.internet.email({ firstName, lastName });
+
+const countries = ['India', 'United States', 'Canada', 'Australia', 'Israel', 'New Zealand', 'Singapore'];
+const randomCountry = countries[Math.floor(Math.random() * countries.length)];
+
+const months = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+export function generateEmail(firstName, lastName) {
+  return faker.internet.email({ firstName, lastName });
 }
 
-export function generatePassword() {  
-    return faker.internet.password({ 
-        length: 10, memorable: true });
-    }
-export function generateName() {  
-    return {    
-        firstName: faker.person.firstName(),    
-        lastName: faker.person.lastName(),  
-    };
+export function generatePassword() {
+  return faker.internet.password({ length: 10, memorable: true });
 }
-export function generateUserDetails() 
-{  
-    return {    
-        company: faker.company.name(),   
-        address: faker.location.streetAddress({ useFullAddress: true }),    
-        city: faker.location.city(),    
-        state: faker.location.state(),    
-        zip: faker.location.zipCode(),   
-        mobile: faker.phone.number('+1 ### ### ####'),  
-    };
+
+export function generateName() {
+  return {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+  };
+}
+
+export function generateUserDetails() {
+  const day = faker.number.int({ min: 1, max: 28 }).toString();
+  const month = months[Math.floor(Math.random() * months.length)];
+  const year = faker.date.birthdate({ min: 18, max: 65, mode: 'age' }).getFullYear().toString();
+
+  return {
+    company: faker.company.name(),
+    address: faker.location.streetAddress({ useFullAddress: true }),
+    city: faker.location.city(),
+    country: randomCountry,
+    state: faker.location.state(),
+    zip: faker.location.zipCode(),
+    mobile: faker.phone.number('+1 ### ### ####'),
+    dateOfBirth: {
+      day,
+      month,
+      year,
+    },
+  };
 }
